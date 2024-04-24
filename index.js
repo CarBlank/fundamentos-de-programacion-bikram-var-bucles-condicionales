7482a1d159d00d7d6eb387f51bb0c93acaa09eea
CarBlank <crblanquer@gmail.com>
2024-04-25 00:05:39 +0200

    update

---
+++ index.js
@@ -0,0 +1,34 @@
+let variableSinValor
+
+let booleano1 = true
+let booleano2 = false
+
+const PI = 3.14
+
+const TAU = (PI * 2)
+
+
+let booleanoAnd = (booleano1 < booleano2)
+
+let booleanoNot = ( !booleano1 )
+
+const booleanoMix0 = (booleano1 || booleano2 && booleano1 || booleano1 && !booleano2)
+
+
+let incrementarDesp = 2
+let resultadoDesp = incrementarDesp++ 
+
+
+let incrementarAntes = 2
+let resultadoAntes = ++incrementarAntes
+
+let contarHasta10_2 
+contarHasta10_2 === 10
+console.log(contarHasta10_2)
+
+for (let contarHasta10_2 = 0; contarHasta10_2 < 10; contarHasta10_2++) {
+    console.log(`Vuelta nº ${contarHasta10_2 + 1}`)
+    }
+    
+    
+
